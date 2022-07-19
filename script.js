3a372cc0e12f34d039f2997e56d08e3520839da6
kunal-arya <85951446+kunal-arya@users.noreply.github.com>
2022-07-19 22:51:55 +0530

    Feat: change Size of the Screen using CSS variables

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const inputs = document.querySelectorAll(".controls input");
 function inputClickHandler() {
     const suffix = this.dataset.sizing || '';
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    console.log(document.documentElement.style)
+    console.log(this.name)
 }
 
 
